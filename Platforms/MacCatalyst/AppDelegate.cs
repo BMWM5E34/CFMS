d94511f18445427f7417588c223bbbbd9a18afd3
Artem <139074869+BMWM5E34@users.noreply.github.com>
2024-04-05 22:49:09 +0300

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CFMS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
